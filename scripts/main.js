c789febc53ee98e609d01cd3dc7fd654d274d35b
Duc Thang Vu <vuducthang91@gmail.com>
2020-03-25 17:53:36 +0100

    fixing

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,15 @@
 /* A script promping user to enter name, surname and favourite colour, and returning a string: name + surname + color + '20' + random number from 0 to 99
 */
 
-console.log('main.js is working')
+console.log('main.js is working');
 
-var name = prompt("What's your name?")
-var surname = prompt("What's your surname?")
-var colour = prompt("What's favourite colour?")
-var ramdonNumber = Math.floor(Math.random() * 100)
+var name = prompt("What's your name?");
+var surname = prompt("What's your surname?");
+var colour = prompt("What's favourite colour?");
+var ramdonNumber = Math.floor(Math.random() * 100);
 
-console.log(name)
-console.log(surname)
-console.log(colour)
+console.log(name);
+console.log(surname);
+console.log(colour);
 
-document.getElementById('new-password').innerHTML = name + surname + colour + '20' + ramdonNumber
+document.getElementById('new-password').innerHTML = name + surname + colour + '20' + ramdonNumber;
