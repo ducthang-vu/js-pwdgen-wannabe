ce8d252c9537fd07ce449154874d0dd48219a0d9
Duc Thang Vu <vuducthang91@gmail.com>
2020-03-25 16:34:27 +0100

    add feature (random number)

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,4 @@
-/* A script promping user to enter name, surname and favourite colour, and returning a string: namesurnamecolour20.
+/* A script promping user to enter name, surname and favourite colour, and returning a string: name + surname + color + '20' + random number from 0 to 99
 */
 
 console.log('main.js script is working')
@@ -6,9 +6,10 @@ console.log('main.js script is working')
 var name = prompt("What's your name?")
 var surname = prompt("What's your surname?")
 var colour = prompt("What's favourite colour?")
+var ramdonNumber = Math.floor(Math.random() * 100)
 
 console.log(name)
 console.log(surname)
 console.log(colour)
 
-document.getElementById('new-password').innerHTML = name + surname + colour + '20'
+document.getElementById('new-password').innerHTML = name + surname + colour + '20' + ramdonNumber
