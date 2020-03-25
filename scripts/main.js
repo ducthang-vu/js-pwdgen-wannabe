3a6bae7760347157290f3b9553a3a6641aa9e5ed
Duc Thang Vu <vuducthang91@gmail.com>
2020-03-25 14:26:51 +0100

    main task completed

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+/* A script promping user to enter name, surname and favourite colour, and returning a string: namesurnamecolour20.
+*/
+
+console.log('main.js script is working')
+
+var name = prompt("What's your name?")
+var surname = prompt("What's your surname?")
+var colour = prompt("What's favourite colou?")
+
+console.log(name)
+console.log(surname)
+console.log(colour)
+
+document.getElementById('new-password').innerHTML = name + surname + colour + '20'
